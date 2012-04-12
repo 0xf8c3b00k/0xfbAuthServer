ab2a275773b5e17c97ff650880cdc19fb66a1ac7
kk1fff <kk1fff@gmail.com>
2012-04-13 01:08:36 +0800

    1. Update server side auth procedure.

--- config.js
+++ config.js
@@ -1,3 +1,27 @@
 exports.config = {
-  'client_waiting_timeout' : 10000, // ms
+  
+  // For 0xfb client.
+  'client_waiting_timeout' : 60000, // ms
+
+  // Facebook app id.
+  'fb_client_id' : '370477596318204',
+
+  // Facebook auth scope.
+  'fb_auth_scope' : [
+    'user_groups',
+    'user_photos',
+    'publish_stream',
+    'friends_photos',
+    'read_stream'
+  ],
+
+  // Path for Facebook's redirect.
+  'fb_auth_result_path' : '/fb_auth_done',
+
+  // Facebook app secret. DON'T SEND TO GITHUB
+  'fb_app_secret' : '',
+
+  // Our app's address.
+  'auth_server_host' : 'https://zeroxf8c3b00k.herokuapp.com',
+
 };
--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var CONFIG = require('./config.js').config;
 var express = require('express');
 var crypto = require('crypto');
+var https = require('https');
 
 // Expecting process:
 // 1. 0xfb client connects to /0xfb_client_token to obtain a token.
@@ -32,6 +33,11 @@ var tokenGenerator = function(cb) {
     cb(s1 + s2);
   });
 };
+
+var generateEncodedRedirectUri = function() {
+  return encodeURIComponent(
+    CONFIG['auth_server_host'] + CONFIG['fb_auth_result_path']);
+}
 
 var generateResponse = function(success, msg, payload) {
   var r = {
@@ -70,29 +76,69 @@ app.get('/0xfb_client_wait', function(req, resp) {
   var q = req.query;
   if (q.hasOwnProperty('t')) {
     var token = q['t'];
-    var waitingStruct = {
-      response: resp,
-      timeout: setTimeout(
-        // Auth client didn't obtain access token in time, 
-        // notify 0xfb client timed out, clear token from
-        // waiting list.
-        function() {
-          console.log('%s timeout', token);
-          resp.send(generateResponse(0, 'timed out'));
-          delete waitingList[token];
-        },
-        CONFIG['client_waiting_timeout'])
-    };
-    console.log("Waiting started. token: %s, timeout: %d ms",
-                token, CONFIG['client_waiting_timeout']);
-    waitingList[token] = waitingStruct;
+
+    if (waitingList.hasOwnProperty(token)) {
+      // Token is already set to wait. We should prevent another client
+      // from replacing it. (Or a security hole may occur.)
+      resp.send(generateResponse(0, 'token is waiting'));
+    } else {
+      // Build a waiting sturct that holds the data structure to communicate
+      // with current client.
+      var waitingStruct = {
+        response: resp,
+        timeout: setTimeout(
+          // Auth client didn't obtain access token in time, 
+          // notify 0xfb client timed out, clear token from
+          // waiting list.
+          function() {
+            console.log('%s timeout', token);
+            resp.send(generateResponse(0, 'timed out'));
+            delete waitingList[token];
+          },
+          CONFIG['client_waiting_timeout'])
+      };
+      console.log("Waiting started. token: %s, timeout: %d ms",
+                  token, CONFIG['client_waiting_timeout']);
+      waitingList[token] = waitingStruct;
+    }
+
   } else {
-    resp.send('Need token');
+    resp.send(generateResponse(0, 'Need token'));
   }
 });
 
-// Called by facebook's forwarding. Handle auth result.
-app.get('/fb_auth_done', function(req, resp) {
+// Called by auth client. With a parameter 't' that holds the token.
+app.get('/auth_client_request', function(req, resp) {
+  var q = req.query;
+
+  if (q.hasOwnProperty('t')) {
+    var token = q['t'];
+    // Make sure that there is a 0xfb client is waiting access_token by
+    // the token.
+    if (waitingList.hasOwnProperty(token)) {
+      // Forward to facebook, start a server side auth.
+      var redirectTo = "https://www.facebook.com/dialog/oauth?" +
+        "client_id=" + CONFIG['fb_client_id'] +
+        "&redirect_uri=" + generateEncodedRedirectUri() +
+        "&scope=" + CONFIG['fb_auth_scope'].join(',') +
+        "&state=" + token;
+      console.log("Dest: %s", redirectTo);
+      resp.header('Location', redirectTo);
+      resp.send(301);
+    } else {
+      // There is no 0xfb client is waiting, may misused or timed out.
+      // Notify user.
+      resp.send(generateResponse(0, 'No such token'));
+    }
+
+  } else {
+    resp.send(generateResponse(0, 'Need token'));
+  }
+});
+
+// Called by facebook's forwarding. Handle auth result, in this step, we should
+// get the 'code' from facebook.
+app.get(CONFIG['fb_auth_result_path'], function(req, resp) {
   var q = req.query;
   
   // state is set when auth client typing, and will be forwarded by
@@ -100,12 +146,44 @@ app.get('/fb_auth_done', function(req, resp) {
   if (q.hasOwnProperty('state')) {
     var token = q['state'];
     if (waitingList.hasOwnProperty(token)) {
+
       var ws = waitingList[token];
       clearTimeout(ws.timeout);
-      if (q.hasOwnProperty('access_token')) {
-        ws.response.send(generateResponse(1, 'auth successed', {
-          'access_token': q['access_token']
-        }));
+
+      if (q.hasOwnProperty('code')) {
+        // We got 'code' from Facebook, so we can use this code to get
+        // access_token.
+        var fbCode = q['code'];
+        https.get({
+          'host' : 'graph.facebook.com',
+          'path' : '/oauth/access_token' +
+            '?client_id=' + CONFIG['fb_client_id'] +
+            '&redirect_uri=' + generateEncodedRedirectUri() +
+            '&client_secret=' + CONFIG['fb_app_secret'] +
+            '&code=' + fbCode
+        }, function(fbResp) {
+
+          var fbRespStr = '';
+          fbResp.on('data', function(buf) {
+            fbRespStr = fbRespStr + buf.toString('utf8');
+          });
+
+          fbResp.on('end', function() {
+            // Parse response
+            console.log(fbRespStr);
+            var fbRespStrPart = fbRespStr.split('&');
+            var access_token = fbRespStrPart[0].split('=')[1];
+            var expires = fbRespStrPart[1].split('=')[1];
+
+            ws.response.send(generateResponse(1, 'auth successed', {
+              'access_token': access_token,
+              'expires' : expires,
+            }));
+          });
+          // Here done the auth procedure.
+
+        });
+
       } else {
         we.response.send(generateResponse(0, 'auth failed'));
       }
@@ -122,7 +200,7 @@ app.get('/fb_auth_done', function(req, resp) {
   }
 });
 
-var port = process.env.PORT || 3000;
+var port = 40680; //process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
